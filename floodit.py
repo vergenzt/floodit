1971b17e80011bf8384ff6e845b93dc9faf52499
Tim Vergenz <vergenzt@gmail.com>
2012-11-28 18:55:51 -0500

    Add floodit.py

---
+++ floodit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+floodit.py
+
+This is an implementation of the game Flood-it, in which the player is presented
+with a grid of colored squares, and they can change the color of the top-left
+contiguous blob.  With each color change, the top-left blob "absorbs" adjacent
+blobs of that color.  The objective is to fill the board with one color.
+"""
+
