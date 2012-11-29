800b17d9af9d3796152a12cb5852d1c015ea826d
Tim Vergenz <vergenzt@gmail.com>
2012-11-28 20:45:08 -0500

    [grid] Implement FloodGrid.__init__

--- grid.py
+++ grid.py
@@ -4,9 +4,43 @@ grid.py
 This file defines classes used for the Flood-it grid.
 """
 
+import random
+
 class FloodGrid(object):
     """A grid of colors."""
-    pass
+
+    COLORS = 'red green blue cyan yellow magenta'.split()
+
+    def __init__(self, width=12, height=12, seed=None):
+        """Initialize a random grid of colors."""
+        self.width = width
+        self.height = height
+        random.seed(seed)
+
+        grid = []
+        for i in xrange(height):
+            grid.append([])
+            for j in xrange(width):
+                color = random.choice(self.COLORS)
+                left,up = None,None
+                if j > 0: left = grid[i][j-1]
+                if i > 0: up = grid[i-1][j]
+                if (j>0 and i>0) and color == left.color == up.color:
+                    left.positions.add((i,j))
+                    left.merge(up)
+                    grid[i-1][j] = left
+                    grid[i].append(left)
+                elif (j>0) and color == left.color:
+                    left.positions.add((i,j))
+                    grid[i].append(left)
+                elif (i>0) and color == up.color:
+                    up.positions.add((i,j))
+                    grid[i].append(up)
+                else:
+                    blob = Blob(color, (i,j))
+                    grid[i].append(blob)
+
+        self.root = grid[0][0]
 
 
 class Blob(object):
