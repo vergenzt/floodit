3db856be17ad8260ff1fe36e15befb05bc343584
Tim Vergenz <vergenzt@gmail.com>
2012-11-28 19:10:23 -0500

    [grid] Add FloodGrid and Blob classes

--- grid.py
+++ grid.py
@@ -4,3 +4,13 @@ grid.py
 This file defines classes used for the Flood-it grid.
 """
 
+class FloodGrid(object):
+    """A grid of colors."""
+    pass
+
+
+class Blob(object):
+    """A contiguous blob of colors within a FloodGrid."""
+    pass
+
+
